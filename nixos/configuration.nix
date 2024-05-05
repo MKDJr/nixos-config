b812bdcac9e8c0347ce1b1a01b246c9dc4d2d8fc
Michael Dawson <michael@dawsonjr.com>
2024-05-04 22:33:48 -0400

    use zotero 6 instead of 7

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -138,7 +138,7 @@
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
     thunderbird
-    zotero_7
+    zotero
     obsidian
     mixxx
     vscode
