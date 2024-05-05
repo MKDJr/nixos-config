08fb53770a5c9ca7200c52a55438d77c3ec2ef3f
Michael Dawson <michael.k.dawson.jr@gmail.com>
2024-05-05 00:06:40 -0400

    use zotero_7

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -145,7 +145,7 @@
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
     thunderbird
-    zotero
+    zotero_7
     obsidian
     mixxx
     vscode
