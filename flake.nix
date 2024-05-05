c0a259b9080a83d27529b2ec9b62e3ec769cdc79
Michael Dawson <michael.k.dawson.jr@gmail.com>
2024-05-05 00:06:16 -0400

    delete home_manager

--- flake.nix
+++ flake.nix
@@ -17,7 +17,6 @@
   outputs = {
     self,
     nixpkgs,
-    home-manager,
     ...
   } @ inputs: let
     inherit (self) outputs;
@@ -30,18 +29,6 @@
         specialArgs = {inherit inputs outputs;};
         # > Our main nixos configuration file <
         modules = [./nixos/configuration.nix];
-      };
-    };
-
-    # Standalone home-manager configuration entrypoint
-    # Available through 'home-manager --flake .#your-username@your-hostname'
-    homeConfigurations = {
-      # Your username@hostname
-      "mkdjr@GandalfV2" = home-manager.lib.homeManagerConfiguration {
-        pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
-        extraSpecialArgs = {inherit inputs outputs;};
-        # > Our main home-manager configuration file <
-        modules = [./home-manager/home.nix];
       };
     };
   };
